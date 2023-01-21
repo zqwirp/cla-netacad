e647eae51eda3a07607977594a314e08d5680cef
aji <wiji@atop.com.tw>
2023-01-21 13:09:34 +0700

    update conversion1

--- conversion1.c
+++ conversion1.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main() {
-  float n;
-  scanf("%f", &n);
-  printf("\n%.2f\n\n", n);
+	float n;
+	scanf("%f", &n);
+
+	if (1 <= n && n < 2) {
+		printf("Very Bad\n");
+	} else if (2 <= n && n < 3) {
+		printf("Bad\n");
+	} else if (3 <= n && n < 4) {
+		printf("Neutral\n");
+	} else if (4 <= n && n < 5) {
+		printf("Good\n");
+	} else if (5 <= n && n < 6) {
+		printf("Very Good\n");
+	} else {
+		printf("BLANK!\n");
+	}
+
+	return 0;
 }
