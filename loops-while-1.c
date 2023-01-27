cc5bbb1ace1b9c48c8ed6b5db68a4fb321596075
aji <wiji@atop.com.tw>
2023-01-27 11:29:45 +0700

    update

---
+++ loops-while-1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+   Write a program that takes two integer numbers and prints their sum. Do this until the user enters 0 (zero) (but print the last sum).
+   Additionally, if the user inputs 99 as the first number and 0 as the second number, just print Finish., and end the program.
+   Use the while loop in your code.
+   Your version of the program must print the same result as the expected output.
+   */
+
+int main() {
+  short a, b, sum;
+
+  do {
+    printf("a = ", a);
+    scanf("%d", &a);
+
+    printf("\n");
+    printf("b = ", b);
+    scanf("%d", &b);
+
+    sum = a + b;
+    printf("\n");
+    printf("sum = %d\n", sum);
+  } while (a != 0 && b != 0);
+
+  printf("\nprogram stopped\n");
+  printf("last sum = %d\n", sum);
+
+  return 0;
+}
+
